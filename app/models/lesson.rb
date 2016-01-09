1d998094d7a5dc9fbd2f7be62c3f1cb3e473a112
herokang <whuyinkang@gmail.com>
2016-01-10 00:55:50 +0800

    lesson 增删改查完成

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,5 +1,7 @@
 class Lesson < ActiveRecord::Base
   belongs_to :teacher
-  has_and_belongs_to_many :students
+  # has_and_belongs_to_many :students
+  #被我注释掉的一句有问题，提示"PG::UndefinedTable: 错误: 关系 "lessons_students" 不存在"
+  #等待suemi修复
   has_many :quizs
 end
