2261fd390f6ebe705b9495f7df12b82c6fd16ce9
liuxubin <871301870@qq.com>
2016-01-09 22:32:06 +0800

    student manage module

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,27 @@ class Student < ActiveRecord::Base
   has_many :home_works
   has_and_belongs_to_many :lessons
   has_one :user
+
+  def self.profile(id, pwd)
+    ret = []
+    @profile = Std.find(id)
+    if @profile == nil || @profile.empty?
+      ret[0] = 'the user not exist'
+      ret[1] = -1
+      return ret
+    else
+      pwd = Digest::SHA1.hexdigest(pwd)
+      if @profile.pwd == pwd
+        ret[0] = 'login success'
+        ret[1] = 0
+        return true
+      else
+        ret[0] = 'the password is not correct'
+        ret[1] = -1
+        return false
+      end
+    end
+
+
+  end
 end
