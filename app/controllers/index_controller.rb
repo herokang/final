b17105494d577f4fa8bd74c5756906ba9b8256d8
herokang <whuyinkang@gmail.com>
2016-01-11 22:13:28 +0800

    注册页面

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,8 +1,13 @@
 class IndexController < ApplicationController
   def index
+    render 'index/login'
   end
 
   def login
+  end
+
+  def userlogin
+    render 'index/login'
   end
 
   def register
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'index/register'
 
+  get 'index/userlogin', to:"index#userlogin"
+
   get 'reports/:quizId', to: "reports#show"
 
   resources :users
